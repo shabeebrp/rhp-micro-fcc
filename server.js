5931c938248b55074eef68c5283a559b555b4bf4
Shabeeb <shabeebrp@gmail.com>
2016-05-18 23:00:55 +0530

    added ip and language

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
-app.get('/',function(req,res){
-    res.end('hello world\n');
+app.get('/api/whoami',function(req,res){
+    var out = {
+        ipaddress:"s",
+        language:"s",
+        software:"s"};
+    out.ipaddress = req.connection.remoteAddress;
+    out.language = req.get('accept-Language');
+    out.software =req.get('user-agent');
+    res.send(out);
 })
 app.listen(process.env.PORT || 3000, function(){
   console.log("Chat server listening at "+(process.env.PORT || 3000));
