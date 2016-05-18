6b7cf77f6a7be30382fe6be4593e1ae532940ca5
Shabeeb <shabeebrp@gmail.com>
2016-05-18 21:04:10 +0530

    initialized repo

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+app.get('/',function(req,res){
+    res.end('hello world\n');
+})
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Chat server listening at "+(process.env.PORT || 3000);
+  console.log("Chat server listening at "+(process.env.PORT || 3000));
 });
